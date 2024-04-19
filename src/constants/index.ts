dd3b8e56fb4d600ba20ab2edb39e2bf797d5fd49
luckys-lnz <lalucky22@gmail.com>
2024-04-19 19:27:57 +0000

    constants

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+import { title } from "process";
+
+export const navLinks =[
+    {
+        id: "about",
+        title: "About"
+    },
+    {
+        id: "reserve",
+        title: "Reserve"
+    },
+    {
+        id: "agreement",
+        title: "Agreement"
+    },
+];
