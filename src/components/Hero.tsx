f368912571b0ea447e9f720f9d20b88579e2cb12
luckys-lnz <lalucky22@gmail.com>
2024-04-25 16:51:15 +0000

    Hero session... added Banna and images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import Image from "next/image";
+import { bannerImg, chinese, cuteSmile } from "@/assets";
 
 function Hero() {
   return (
     <div className="mt-20 px-6">
-      <div className="h-[500px] w-1/2 rounded shadow bg-slate-300">
-        <div className="flex justify-center items-center">
-          <h1 className="font-poppins text-[64px] font-[600px] leading-[64px] ">
+      <div className="flex grid-cols-2 justify-center items-start">
+        <div className="">
+          <h1 className="font-poppins font-[600px] text-[32px] leading-[38px] pb-[25px]">
             Reserve Your Ideal Holiday
           </h1>
+          <span className="font-poppins font-[600px] text-[20px] leading-6">
+            Lets get Aquinted
+          </span>
+          <p className="font-poppins font-[400px] text-[13px] leading-[18px]">
+            Auctor nibh hendrerit gravida aenean ut dis posuere bibendum at.
+            Mattis velit at adipiscing ut eu.
+          </p>
         </div>
+        <Image
+          src={bannerImg}
+          alt="banner_img"
+          className="w-[100px] h-[220px]"
+        />
       </div>
     </div>
   );
