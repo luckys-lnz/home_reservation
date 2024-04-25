3d76806c5799a7f750cfb5961eb941d4cda7e2c0
luckys-lnz <lalucky22@gmail.com>
2024-04-25 16:53:03 +0000

    added image to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,15 @@
 import logo from "./Logo.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import bannerImg from "./orange_shirt.png"
+import chinese from "./cute_chi.png"
+import cuteSmile from "./cute_smile.png"
 
 export{
     logo,
     menu,
-    close
+    close,
+    bannerImg,
+    chinese,
+    cuteSmile,
 }
